5e82e9e0f052c7fd8eaef4639954d1c6cb6b9783
jaceksienski <jacek.sienski.jr@gmial.com>
2022-05-14 11:16:29 +0200

    Last video- BiConsumer, next to watch- Predicate

--- src/main/java/functionalInterface/_Function.java
+++ src/main/java/functionalInterface/_Function.java
@@ -1,20 +1,45 @@
 package functionalInterface;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class _Function {
     public static void main(String[] args) {
+
+        //Function takes 1 argument ad produces 1 result
         int increment = incrementByOne(0);
         System.out.println(increment);
 
-        int increment2 = incrementByOneFunction.apply(0);
+        int increment2 = incrementByOneFunction.apply(1);
         System.out.println(increment2);
+
+        int multiplyBy10 = multiplyBy10Function.apply(increment2);
+        System.out.println(multiplyBy10);
+
+        Function<Integer, Integer> addBy1AndThenMultiplyBy10 =
+                incrementByOneFunction.andThen(multiplyBy10Function);
+        System.out.println(addBy1AndThenMultiplyBy10.apply(4));
+
+        //BiFunction takes 2 argument ad produces 1 result
+        System.out.println(incrementByOneAndMultiply(4, 100));
+        System.out.println(incrementByOneAndMultiplyBiFunction.apply(4, 100));
     }
 
     static Function<Integer, Integer> incrementByOneFunction =
             number -> number + 1;
 
+    static Function<Integer, Integer> multiplyBy10Function = number -> number * 10;
+
     static int incrementByOne(int number) {
         return number + 1;
     }
+
+    static BiFunction<Integer, Integer, Integer> incrementByOneAndMultiplyBiFunction =
+            (numberToIncrementByOne, numberToMultiblyBy)
+                    -> (numberToIncrementByOne + 1) * numberToMultiblyBy;
+
+    static int incrementByOneAndMultiply(int number, int numToMultiplyBy) {
+        return (number + 1) * numToMultiplyBy;
+    }
+
 }
